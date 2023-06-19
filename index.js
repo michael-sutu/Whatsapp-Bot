9f0caf427b1ac36c21e4e269afa967ee6b35572f
MikeJMS8910 <61876275+MikeJMS8910@users.noreply.github.com>
2023-06-19 13:29:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -63,8 +63,18 @@ const write = function (docId, newData) {
 let getResponse = function (messageHistory, newMessage) {
     return new Promise(function (resolve, reject) {
         let gptMessages = [{ "role": "user", "content": "You are a friendly tool to help people follow their path in Christianity. Here are some rules and examples that you should follow: You should always provide advice based on the essence of Bible verses. While you may add secular advice that aligns with the teachings of the verse, it should never give opinionated secular advice. You empathize briefly before providing the verse. You can personalize your messages by using the word you. For sensitive topics, you should focus on providing Bible verses and explaining their meanings, without offering extensive secular advice. In responses to queries, you should focus on a primary verse that is most relevant to the query or topic and a summary of what the verse means. In dealing with sensitive or controversial topics, you should exercise caution. You should offer uplifting verses, refrain from subjective advice, and recommend that the user talk to a trusted individual about their issues. If a query is unrelated to the Bible, you should remind users that your there to assist with Bible-related questions and cannot provide expertise in unrelated areas. You should avoid using first-person language to maintain your role as a tool or resource, not a conversational partner. For example, instead of saying 'I suggest reading this verse...', the you could say 'Consider this verse...' or This verse might be helpful...'. The language should still be friendly and accessible, but you should not create an impression of having your own thoughts or feelings. Do you understand?" }, { "role": "assistant", "content": "Yes, I understand and will follow all of these rules and examples that you have set for me." }]
-        for(let i = 0; i < messageHistory.length; i++) {
-            gptMessages.push(messageHistory[i])
+        if(messageHistory.length <= 10) {
+            for(let i = 0; i < messageHistory.length; i++) {
+                gptMessages.push(messageHistory[i])
+            }
+        } else {
+            for(let i = 0; i < 5; i++) {
+                gptMessages.push(messageHistory[i])
+            }
+
+            for(let i = messageHistory.length - 5; i <= messageHistory.length; i++) {
+                gptMessages.push(messageHistory[i])
+            }
         }
         gptMessages.push(newMessage)
     
@@ -113,12 +123,12 @@ client.on('message', async (message) => {
             let newStep = 0
             if(user !== undefined) {
                 if(user.Step == 0) {
-                    client.sendMessage(message.from, `Hello, ${message.body}. What version of the bible do you prefer?`)
-                    newResponse = `Hello, ${message.body}. What version of the bible do you prefer?`
+                    client.sendMessage(message.from, `Thanks, ${message.body}. And do you have a preference for the bible version? e.g. NIV / KJV. (It's also fine if you don't)`)
+                    newResponse = `Thanks, ${message.body}. And do you have a preference for the bible version? e.g. NIV / KJV. (It's also fine if you don't)`
                     newStep = 1
                 } else if(user.Step == 1) {
-                    client.sendMessage(message.from, `Noted, tell me a little about whats going on in your life.`)
-                    newResponse = `Noted, tell me a little about whats going on in your life.`
+                    client.sendMessage(message.from, `Great - you're all set up. Is there anything on your mind today?`)
+                    newResponse = `Great - you're all set up. Is there anything on your mind today?`
                     newStep = 2
                 } else {
                     let response = await getResponse(user.History, {"role": "user", "content": message.body})
@@ -151,8 +161,9 @@ app.get("/start", async (req, res) => {
     try {
         let current = await read()
         if(current[req.query.number] == undefined) {
-            await chat.sendMessage("Hello. I'm Shepard, whats your name?")
-            write(req.query.number, {"History": [{"role": "assistant", "content": "Hello. I'm Shepard, whats your name?"}], "Number": req.query.number, "Step": 0})
+            await chat.sendMessage("Welcome to Shepherd! It's a pleasure to have you on board. You can message at any time with questions or queries. All responses are all based on the Word of the Bible. Let's grow together 🌱.")
+            await chat.sendMessage("What's your name?")
+            await write(req.query.number, {"History": [{"role": "assistant", "content": "Welcome to Shepherd! It's a pleasure to have you on board. You can message at any time with questions or queries. All responses are all based on the Word of the Bible. Let's grow together 🌱."}, {"role": "assistant", "content": "What's your name?"}], "Number": req.query.number, "Step": 0})
         } else {
             await chat.sendMessage("Looks like you tried to initiate a chat with me again. You can just directly talk to me here.")
         }
